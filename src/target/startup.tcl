fcca5568fa7e084d24fba71640c60ea62731d026
Matthias Welwarsky <matthias.welwarsky@sysgo.com>
2018-10-01 14:03:19 +0200

    target: initialize DAPs after reset
    Initialize DAPs not only on first connect but also whenever the target
    is reset through SRST.
    
    Change-Id: I1e3ff33de3bba824f174b077ed3a360efd76f32d
    Signed-off-by: Matthias Welwarsky <matthias.welwarsky@sysgo.com>
    Signed-off-by: Tomas Vanek <vanekt@fbl.cz>

--- src/target/startup.tcl
+++ src/target/startup.tcl
@@ -157,6 +157,9 @@ proc ocd_process_reset_inner { MODE } {
 		# Use TRST or TMS/TCK operations to reset all the tap controllers.
 		# TAP reset events get reported; they might enable some taps.
 		init_reset $MODE
+
+		# after resetting the JTAG chain, re-initialize all existing DAPs
+		dap init
 	}
 
 	# Assert SRST, and report the pre/post events.
@@ -180,6 +183,7 @@ proc ocd_process_reset_inner { MODE } {
 	reset_deassert_initial $MODE
 	if { !$early_reset_init } {
 		if [using_jtag] { jtag arp_init }
+		dap init
 	}
 
 	foreach t $targets {
